99a030dfdcca81300cf5bdf08cc499deaa657006
Joseph Smith <81628842+JoeSmith2099@users.noreply.github.com>
2023-10-08 07:32:59 -0400

    Fibonacci Sequence Corrected
    Removed hardcoded list from Fibonacci program and added loop

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,18 @@
-#This program takes an integer up to 30 and prints out that amount of numbers in the fibonacci sequnce.
-#I want to make it so it can print out any amount of numbers in the sequence, next.
-n = int(input("How many numbers in the fibonacci sequence do you want? Don't exceed 30. "))
+#This program takes an integer and prints out that amount of numbers in the fibonacci sequnce.
+#THe first two numbers in the sequnce are hardcoded and I can't figure out what's going on wrong.
+n = int(input("How many numbers in the fibonacci sequence do you want?  "))
 
-fibonacci_list=[0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89,144,233,377,610,987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229]
+last_num =0
+current_num=1
 
-if n < 31:
-  print("This program will print out the first " + str(n) + " numbers of the fibonacci sequence.")
-  for i in range(0,n):
-    print (fibonacci_list[i])
-else:
-  print("Don't exceed 30. ")
+print(last_num)
+print(current_num)
+for i in range (0,n):
+  sum = last_num + current_num
+  last_num = current_num
+  current_num = sum
+  print(sum)
+
+
+
+print("This program will print out the first " + str(n) + " numbers of the fibonacci sequence.")
